bbdbfffcd04d770547bb9e02be28d60eea41f944
Mia Battaglia <miabattaglia12@gmail.com>
2025-01-26 18:02:47 -0300

    cambiar el fondo mobile del home, hacer formulario y empezar el juego

---
+++ js/escuchar.js
@@ -0,0 +1,24 @@
+//window open y blank para abrir en otra penstaña
+  let boton1 = document.querySelector("#intongues");
+  // in tongues
+  boton1.addEventListener('click', () =>
+      window.open('https://open.spotify.com/track/7wvwXi9Z66dcH5t202vVkN?si=lFCiSZL3QDyVDG3TW8lVWw', '_blank')
+  );
+
+  let boton2 = document.querySelector("#ballads1");
+  // ballads1
+  boton2.addEventListener('click', () =>
+      window.open('https://open.spotify.com/track/3u9RHP8IpKPZh7bKHw83Ci?si=K3A7jRMlROO3WBQMBxFKFQ&context=spotify%3Aalbum%3A5mIImcsuqpiSXg8XvFr81I', '_blank')
+  );
+  
+  let boton3 = document.querySelector("#nectar");
+  // nectar
+  boton3.addEventListener('click', () =>
+      window.open('https://open.spotify.com/track/0haqbQIGxiZxt1U9WODb9q?si=OqMrgBs9SAy_KKAjacEadQ&context=spotify%3Aalbum%3A65edimIChzNNK8VGn56pIK', '_blank')
+  );
+  
+  let boton4 = document.querySelector("#smithereens");
+  // smithereens
+  boton4.addEventListener('click', () =>
+      window.open('https://open.spotify.com/track/4imZDfJ9jooxgCGdx9j3bf?si=Ih-vOrjxRxe0VCnKE54drw&context=spotify%3Aalbum%3A39VuC5rYQHAnR6xQwm1WDk', '_blank')
+  );
